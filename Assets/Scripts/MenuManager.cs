5e5470527cbe0acde3085fb9deb3543477ad4ab4
VicCTS <victordm.cts@gmail.com>
2024-03-05 10:35:40 +0100

    ultima clase

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour
+{
+    public Text scoreText;
+    public int score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadScore();
+    }
+
+    void LoadScore()
+    {
+        scoreText.text = "Puntacion: " + score.ToString();
+    }
+
+    public void LoadFirstLevel()
+    {
+        SceneManager.LoadScene("Nivel 1");
+    }
+}
